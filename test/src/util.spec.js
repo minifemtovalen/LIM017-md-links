a3e312ceff2a90de2d87cfe0fb6cafd9f5dfded0
Valentina Patti <valentinapatti23@gmail.com>
2022-05-24 11:57:16 -0500

    add tests for story 2 and 3

--- test/src/util.spec.js
+++ test/src/util.spec.js
@@ -1,4 +1,9 @@
-const {convertToAbsolute} = require('../../src/util.js');
+const {
+  convertToAbsolute,
+  isDirectory,
+  isValidPath,
+  isMdFile,
+} = require('../../src/util.js');
 
 describe('convertToAbsolute', () => {
   const path1 = '../LIM017-social-network/README.md';
@@ -14,4 +19,42 @@ describe('convertToAbsolute', () => {
   });
 });
 
+describe('isDirectory', () => {
+  const directoryPath = '/home/ozzy/Documents/kitten-directory';
+  const notADirectory = '/home/ozzy/Documents/cat.jpeg';
+
+  it('Should return true if the path is a directory', () => {
+    expect(isDirectory(directoryPath)).toBe(true);
+  });
+
+  it('Should return false if the path is not a directory', () => {
+    expect(isDirectory(notADirectory)).toBe(false);
+  });
+});
+
+describe('isValidPath', () => {
+  const path = '/home/ozzy/Desktop/cats-are-awesome.md';
+
+  it('Should return true if the path is valid', () => {
+    expect(isValidPath(path)).toBe(true);
+  });
+
+  it('Should return false if the path is not valid', () => {
+    expect(isValidPath('/cats-are-dumb')).toBe(false);
+  });
+});
+
+describe('IsMdFile', () => {
+  const firstFile = '/home/ozzy/Documents/kitten-directory/cats-are-better.md';
+  const secondFile =
+  '/home/ozzy/Documents/kitten-directory/dogs-are-better.txt';
+
+  it('Should return true if the file ext is .md', () => {
+    expect(isMdFile(firstFile)).toBe(true);
+  });
+
+  it('Should return false if the file ext is not .md', () => {
+    expect(isMdFile(secondFile)).toBe(false);
+  });
+});
 
